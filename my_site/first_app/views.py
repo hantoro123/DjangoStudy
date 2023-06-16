3cba0c4432fa0ddeea37ba50195b600f9fe1a981
SangJun <grw05177@naver.com>
2023-06-17 06:09:08 +0900

    filter of templates

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -9,7 +9,7 @@ def simple_view(request):
 
 def variable_view(request):
 
-    my_var = {'first_name':'Rosalind', 'last_name':'Franklin',
+    my_var = {'first_name':'rosaLind', 'last_name':'franklin',
               'some_list':[1,2,3], 'some_dict': {'inside_key':'inside_value'}}
 
     return render(request, 'first_app/variable.html',context=my_var) 
