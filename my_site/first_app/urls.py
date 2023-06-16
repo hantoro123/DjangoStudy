bd930a9e6cbd224b465da30c6fe5e0bca58f78b3
SangJun <grw05177@naver.com>
2023-06-16 22:29:12 +0900

    variable of Templates

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.simple_view) # domain.com /first_app
-
+    path('', views.simple_view), # domain.com /first_app
+    path('variable/', views.variable_view),
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -6,3 +6,10 @@ from django.urls import reverse
 
 def simple_view(request):
     return render(request,'first_app/example.html') #.html
+
+def variable_view(request):
+
+    my_var = {'first_name':'Rosalind', 'last_name':'Franklin',
+              'some_list':[1,2,3], 'some_dict': {'inside_key':'inside_value'}}
+
+    return render(request, 'first_app/variable.html',context=my_var) 
