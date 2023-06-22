e19143fea243587c50fd41bf9fd136b5e862d34a
SangJun Han <grw05177@naver.com>
2023-06-22 20:02:25 +0900

    Tags and URL name of the tamplate

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+# register the app namespace
+# URL Names
+app_name = 'first_app'
+
 urlpatterns = [
-    path('', views.simple_view), # domain.com /first_app
-    path('variable/', views.variable_view),
+    path('', views.simple_view, name='example'), # domain.com /first_app
+    path('variable/', views.variable_view, name='variable'),
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -8,8 +8,4 @@ def simple_view(request):
     return render(request,'first_app/example.html') #.html
 
 def variable_view(request):
-
-    my_var = {'first_name':'rosaLind', 'last_name':'franklin',
-              'some_list':[1,2,3], 'user_logged_in':False}
-
-    return render(request, 'first_app/variable.html',context=my_var) 
+    return render(request, 'first_app/variable.html') 
