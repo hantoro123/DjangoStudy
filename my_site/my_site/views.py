e1af65d438c50e95eae2bd2a0a90ae20c76fb135
SangJun Han <grw05177@naver.com>
2023-06-23 22:02:58 +0900

    Re-change to 404.html

--- my_site/my_site/views.py
+++ my_site/my_site/views.py
@@ -8,5 +8,7 @@ def home_view(request):
     return HttpResponse("HOME")
 
 def my_custom_page_not_found_view(request,exception):
-    return render(request, 'error_view.html',status=404)
+    return render(request, '404.html',status=404)
+    # 이 방법도 있지만 이런 방법 없이 404.html이라고
+    # 작성하는 것이 더 강력하고 권장하는 방법이다.
 
