dd298cf7236d5f01bee51250441688324eddc03f
SangJun Han <grw05177@naver.com>
2023-06-23 21:45:02 +0900

    404 page chages and DEBUG = False

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g735g*f-x3mfr^-s&vdn!rm78%e$09^03shq@f(zu!7^g#&j#c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
