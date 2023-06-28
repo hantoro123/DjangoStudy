9d7199e11655129523c30905dca78ead143a95fb
SangJun <grw05177@naver.com>
2023-06-28 16:59:32 +0900

    Additional makemigrations for heartrate values and set default as modification

--- second_site/office/models.py
+++ second_site/office/models.py
@@ -5,6 +5,7 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+    heartrate = models.IntegerField(default=60) # null=True로 널값으로 초기값 설정 가능
 
     def __str__(self):
 
