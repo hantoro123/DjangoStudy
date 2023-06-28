49c00ebdf401cef2f25223c941c2b12c9cc24538
SangJun <grw05177@naver.com>
2023-06-28 17:05:47 +0900

    Run makemigrations and migrate with validators in age and heartrate

--- second_site/office/models.py
+++ second_site/office/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
-    heartrate = models.IntegerField(default=60) # null=True로 널값으로 초기값 설정 가능
+    age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(120)])
+    heartrate = models.IntegerField(default=60, validators=[MinValueValidator(1),MaxValueValidator(300)]) # null=True로 널값으로 초기값 설정 가능
 
     def __str__(self):
 
