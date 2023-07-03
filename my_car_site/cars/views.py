de326e20eed7574cd303af771e507e6cbe8150fd
SangJun <grw05177@naver.com>
2023-07-03 15:48:39 +0900

    Modify delete.html and create a views.py delete function

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -21,4 +21,14 @@ def add(request):
         return render(request,'cars/add.html')
 
 def delete(request):
-    return render(request,'cars/delete.html')
+    if request.POST:
+        # delete the car
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!')
+            return redirect(reverse('cars:list'))
+    else:
+        return render(request,'cars/delete.html')
