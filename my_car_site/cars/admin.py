90a8f45a41872cc2a2fba82ce49180e0862b1dcd
SangJun Han <grw05177@naver.com>
2023-07-04 16:31:37 +0900

    Customizing the admin page

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from cars.models import Car
 # Register your models here.
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('TIME INFOMATION', {'fields':['year']}),
+        ('CAR INFORMATION', {'fields':['brand']})
+    ]
 
+admin.site.register(Car, CarAdmin)
