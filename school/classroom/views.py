d2990444ce7015f505672d1ee7b16f4c2bb38c22
SangJun <grw05177@naver.com>
2023-07-14 02:43:10 +0900

    Connect contact page and redirect to class-based view

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
+
+from classroom.form import ContactForm
 
 # Create your views here.
 def home_view(request):
@@ -10,3 +13,15 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    # success URL?
+    success_url = reverse_lazy("classroom:thank_you")
+
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
