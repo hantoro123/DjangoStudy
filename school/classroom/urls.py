cc11291b1b4053810172037313ce6f85e9823377
SangJun <grw05177@naver.com>
2023-07-14 02:08:21 +0900

    connecting url to a class-based view

--- school/classroom/urls.py
+++ school/classroom/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import HomeView, ThankYouView
 
 app_name = 'classroom'
 # domain.com/classroom
 urlpatterns = [
-    path('',home_view, name='home')
+    path('', HomeView.as_view() , name='home'),
+    path('thank_you/', ThankYouView.as_view(), name='thank_you')
 ]
